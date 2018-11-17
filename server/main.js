8765ab2b5a078397e55533a3b7f6eef364566dde
Aksa Mohammadi <aksa0004@gmail.com>
2018-11-17 13:07:20 +0530

    Add socket.io to an app

--- server/main.js
+++ server/main.js
@@ -1,15 +1,30 @@
 const path = require('path')
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
 const port = process.env.PORT || 4000;
 const way = path.join(__dirname+'/../public')
 var app = express();
 console.log(__dirname+"/../public")
 app.use(express.static(way))
+const server = http.createServer(app)
+const io = socketIO(server)
+
+io.on('connection',(sock)=>{
+	console.log('new joinee ...welocome')
+
+	// sock.on('connect',()=>{
+	// 	console.log('user is connected')
+	// })
+	sock.on('disconnect',()=>{
+		console.log('bye to disconnected')
+	})
+})
 // app.get('/',(req,res)=>{
 // 	res.send(index.html)
 // })
-app.listen(port,()=>{
+server.listen(port,()=>{
 	console.log('yeah...done')
 })
 
